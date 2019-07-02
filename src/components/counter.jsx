9d33db2959976c1c703570af55cbf5ab83a7f6fb
Mariia <mariiapopovych@gmail.com>
2019-07-02 00:48:58 -0700

    passing children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state={
-        count: 0,
-        tags:[]
+        value: this.props.value
+       
   
     };
 
@@ -23,20 +23,21 @@ class Counter extends Component {
 
     getBadge1Classes() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
     formatCount(){
-        const {count} = this.state;
-        return count === 0? "Zero": count;
+        const {value} = this.state;
+        return value === 0? "Zero": value;
     }
     handleIncrement = product => {
-        this.setState({count: this.state.count+1}) 
+        this.setState({value: this.state.value+1}) ;
     };
   
     render() { 
         return (
         <div>
+            {this.props.children}
        <span  className={this.getBadge1Classes()}>{this.formatCount()}
          </span>
          <button onClick={() => this.handleIncrement()}
