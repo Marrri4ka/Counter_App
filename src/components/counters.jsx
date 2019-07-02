b33af15da67685e8a3651546fb4e1eefdb66815d
Mariia <mariiapopovych@gmail.com>
2019-07-02 12:06:00 -0700

    add delete function

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,11 +10,21 @@ class Counters extends Component {
             {id: 5, value: 0},
 
         ]
-     }
+     };
+
+     handleDelete  = counterId => {
+        const counters = this.state.counters.filter(c=>c.id !== counterId);
+        this.setState ({ counters});
+
+     };
     render() { 
         return (
              <div>{this.state.counters.map(counter=>(
-             <Counter key = {counter.id} value={counter.value}>
+             <Counter
+              key = {counter.id}
+              onDelete={this.handleDelete}
+             value={counter.value}
+             id={counter.id}>
                  <h4>Counter #{counter.id}</h4>
                  </Counter>
                  ))}</div> 
